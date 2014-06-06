4b92981c42fb894250eb4d6d1574408936ccb9cc
drtagkim <masan.korea@gmail.com>
2014-06-06 13:43:36 +0800

    documentation

--- NKM_TOYBOX/algorithm.py
+++ NKM_TOYBOX/algorithm.py
@@ -22,4 +22,19 @@ def linear_shock(contribution_table,shock):
     ep = NPRD.rand(table_dim[0],table_dim[1],table_dim[2])
     new_table = (1 - shock) * contribution_table + shock * ep
     return new_table
+def incentive_structure(incentive_base, c_i, which_a, which_b, part_idx):
+    """
+which_a <- [...]
+which_b <- [...]
+    """
+    N = float(len(which_a) + len(which_b))
+    assert N > 0, "No incentive at all"
+    import numpy as NP
+    sum_of_a = c_i[which_a].sum()
+    sum_of_b = c_i[which_b].sum()
+    if part_idx < 1:
+        F_d = (sum_of_a + incentive_base * sum_of_b) / N
+    else:
+        F_d = (incentive_base * sum_of_a) / N
+    return F_d
 # END OF PROGRAM #
